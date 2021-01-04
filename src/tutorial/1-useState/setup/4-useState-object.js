2babbe9e9fcbaf725b5da386d69882a0edae2e30
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2021-01-04 09:44:38 +0100

    finished use state object practice

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Mersad",
+    age: 31,
+    message: "Hello World, this is Mersad",
+  });
+
+  const changeMessage = () => {
+    setPerson({ ...person, message: "Hello Mersad!" });
+  };
+  return (
+    <React.Fragment>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button type="button" className="btn" onClick={changeMessage}>
+        Change Message
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateObject;
