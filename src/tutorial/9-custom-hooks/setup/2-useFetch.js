97b8b8e34cdc1a975380578d99eac886ade2cdeb
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2021-02-05 10:19:54 +0100

    finished custom hooks practice

--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-export const useFetch = () => {};
+export const useFetch = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(url);
+      const newData = await response.json();
+      setData(newData);
+      setLoading(false);
+    };
+
+    getData();
+  }, [url]);
+
+  return { loading, data };
+};
